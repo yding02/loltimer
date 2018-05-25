507e2b6d9853c2e5be1aee423cffddef49991a72
Yifeng Ding <yding12321@gmail.com>
2018-05-24 17:27:50 -0700

    updated constants

--- constants.py
+++ constants.py
@@ -1,18 +1,28 @@
 """
 ROLES
 """
-TOP = ('top',)
+TOP = ('top', )
 MID = ('mid', 'm', 'middle')
 JG = ('jungle', 'jg', 'j')
 BOT = ('bot', 'ad', 'adc', 'a', 'b')
 SUPP = ('supp', 'support', 's', 'sup')
+ALL_ROLE = ('all', )
 
 TOP_CANON = 'top'
 MID_CANON = 'mid'
 JG_CANON = 'jg'
 BOT_CANON = 'bot'
 SUPP_CANON = 'supp'
+NO_ROLE_CANON = 'norole'
+ALL_ROLE_CANON = 'allrole'
 
+ROLES = { TOP : TOP_CANON,
+          MID : MID_CANON,
+          JG : JG_CANON,
+          BOT : BOT_CANON,
+          SUPP : SUPP_CANON,
+          ALL_ROLE : ALL_ROLE_CANON
+        }
 """
 SUMMONERS
 """
@@ -35,6 +45,18 @@ TELEPORT_CANON = 'teleport'
 SMITE_CANON = 'smite'
 CLENSE_CANON = 'clense'
 IGNITE_CANON = 'ignite'
+NO_SUMM_CANON = 'nosumm'
+
+SUMMONERS = { HEAL : HEAL_CANON,
+              GHOST : GHOST_CANON,
+              BARRIER : BARRIER_CANON,
+              EXHAUST : EXHAUST_CANON,
+              FLASH : FLASH_CANON,
+              TELEPORT : TELEPORT_CANON,
+              SMITE : SMITE_CANON,
+              CLENSE : CLENSE_CANON,
+              IGNITE : IGNITE_CANON,
+            }
 
 """
 COOLDOWNS
@@ -48,6 +70,7 @@ COOLDOWNS = { HEAL_CANON : 270,
               SMITE_CANON : 90,
               CLENSE_CANON : 210,
               IGNITE_CANON : 210,
+              NO_SUMM_CANON : 0,
             }
             
 """
@@ -55,4 +78,19 @@ COMMANDS
 """  
 TRACK = ('track', 't')
 CDR = ('cdr', 'cooldown', 'cd')
-SUMMS = ('summs', 'summ', 's')          
+SUMMS = ('summs', 'summ', 's')
+
+TRACK_CANON = 'track'
+CDR_CANON = 'cdr'
+SUMMS_CANON = 'summs'
+NO_COMMAND_CANON = 'nocommand'
+
+COMMANDS = {TRACK : TRACK_CANON, 
+            CDR : CDR_CANON, 
+            SUMMS : SUMMS_CANON
+           }
+
+"""
+MISC
+"""
+DEFAULT_OFFSET = 10
